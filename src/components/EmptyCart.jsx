8401579df13d118eb6bba68b82244d730155fcfa
Angel Alberto Albistro <angelalbistro@gmail.com>
2025-06-30 23:11:29 -0300

    cambio en estilos

--- src/components/EmptyCart.jsx
+++ src/components/EmptyCart.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
     
     const EmptyCart = () => {
       return (
-          <div>
-              <h2>Tu Carrito esta vacio como su corazon</h2>
+          <div style={{ textAlign: "center", marginTop: "1.5rem" }}>
+              <h2>Tu Carrito está vacio, como su corazon</h2>
               <h3>bla bla ver productos</h3>
               <Link className='btn btn-dark' to={'/'}>Ir a home</Link>
         </div>
